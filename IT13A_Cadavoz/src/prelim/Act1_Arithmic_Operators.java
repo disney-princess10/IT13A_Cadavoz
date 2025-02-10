6ff49edc673e2dfbdfd1e2b546eec1456034d605
Arriel Denver T. Cadavoz <cadavoz.arrieldenver@gmail.com>
2025-02-10 18:42:22 +0800

    "Added Java Application"

---
+++ IT13A_Cadavoz/src/prelim/Act1_Arithmic_Operators.java
@@ -0,0 +1,25 @@
+
+package prelim;
+
+
+public class Act1_Arithmic_Operators {
+    
+    public static void main (String[]args){
+    int firstnumber= 125;
+    int secondnumber= 24;
+    
+    int addition= firstnumber + secondnumber;
+    int subtraction= firstnumber - secondnumber;
+    int multiply= firstnumber * secondnumber;
+    int divide= firstnumber / secondnumber;
+    int module= firstnumber % secondnumber;
+    
+    System.out.println("125 + 24 = " + addition);
+    System.out.println("125 - 24 = " + subtraction);
+    System.out.println("125 x 24 = " + multiply);
+    System.out.println("125 / 24 = " + divide);
+    System.out.println("125 mod 24 = " + module);
+            
+            
+    }
+}
